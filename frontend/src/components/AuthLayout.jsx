8a3aaa97db54d6e4754e5be8238377aac39ae7ab
Chirag <chirag.sharma26262@gmail.com>
2024-07-13 10:41:34 +0530

    fixed some issues

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -72,7 +72,7 @@ const RegisterForm = () => (
 const LoginPrompt = ({ toggleView }) => (
     <div className="p-8 h-full flex flex-col justify-center items-center text-white">
         <h2 className="text-2xl font-bold mb-4">Welcome back!</h2>
-        <p className="mb-4 text-center">Already a member of CashCompass? Log in now with your credentials to access your account and manage your finances.</p>
+        <p className="hidden md:block mb-4 text-center">Already a member of CashCompass? Log in now with your credentials to access your account and manage your finances.</p>
         <button onClick={toggleView} className="p-2 bg-transparent font-bold border-2 ring-1 ring-gray-200 rounded-md">SIGN IN</button>
     </div>
 );
@@ -80,7 +80,7 @@ const LoginPrompt = ({ toggleView }) => (
 const RegisterPrompt = ({ toggleView }) => (
     <div className="p-8 h-full flex flex-col justify-center items-center text-white">
         <h2 className="text-2xl font-bold mb-4">Hello there!</h2>
-        <p className="mb-4 text-center">New to CashCompass? Sign up now to unlock powerful features, personalized insights, and secure expense tracking. Join us and take the first step towards financial freedom!</p>
+        <p className="mb-4 hidden md:block text-center">New to CashCompass? Sign up now to unlock powerful features, personalized insights, and secure expense tracking. Join us and take the first step towards financial freedom!</p>
         <button onClick={toggleView} className="p-2 bg-transparent font-bold border-2 ring-1 ring-gray-200 rounded-md">SIGN UP</button>
     </div>
 );
